90323022f9f4b39f63c78f3154a6353a1e126365
Jannes <jannes.luca@hotmail.com>
2021-12-08 19:41:48 +0100

    added pause and quit input

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour
+{
+    private bool paused = false;
+    private object GameManagment;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Pause") && !paused ) pause();
+        else if(Input.GetButtonDown("Pause") && paused) resume();
+        if(Input.GetButtonDown("Quit")) quit();
+    }
+
+    private void quit()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    private void resume()
+    {
+        paused = false;
+        Time.timeScale = 1f;
+        transform.Find("PauseScreen").gameObject.SetActive(false);
+    }
+
+    private void pause()
+    {
+        paused = true;
+        Time.timeScale = 0f;
+        transform.Find("PauseScreen").gameObject.SetActive(true);
+    }
+
+
+}
